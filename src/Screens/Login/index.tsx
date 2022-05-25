dae95099b5ced789ada302e43140484419ac5e64
menuann <llatmos88ll@gmail.com>
2022-05-25 23:03:10 +0900

    kakaoLogin

--- src/Screens/Login/index.tsx
+++ src/Screens/Login/index.tsx
@@ -1,15 +1,12 @@
-import React, {useContext, useEffect} from 'react';
-import {StackNavigationProp} from '@react-navigation/stack';
+import {
+    KakaoOAuthToken,
+    login
+  } from '@react-native-seoul/kakao-login';
+import React, {useState} from 'react';
 import {Text, View, TextInput,Button, StyleSheet, TouchableOpacity } from 'react-native';
-
-import {UserContext} from '~/Context/User';
+import {StackNavigationProp} from '@react-navigation/stack';
 
 import Basic1 from '~/Components/Basic1';
-
-type NavigationProp = StackNavigationProp<LoginNaviParamList, 'Login'>;
-interface Props{
-    navigation: NavigationProp;
-}
 
 const styles = StyleSheet.create({
     button: {
@@ -26,20 +23,28 @@ const styles = StyleSheet.create({
     }
 });
 
-const Login = ({ navigation}: Props) => {
-    const {login} = useContext<IUserContext>(UserContext);
-    return(
+  const Login = () => { 
+    const [result, setResult] = useState<string>('');
+  
+    const signInWithKakao = async (): Promise<void> => {
+      const token: KakaoOAuthToken = await login();
+  
+      setResult(JSON.stringify(token));
+    };
+  
+   
+    return (
         <View>
         <Text style ={{ fontSize: 100}}>Dooby</Text>
         <TextInput style={{marginBottom: 16}} placeholder="이메일" />
         <TextInput style={{marginBottom: 16}} placeholder="비밀번호" />
-        <TouchableOpacity onPress={() => navigation.navigate('Basic1')}>
+        <TouchableOpacity onPress={() => signInWithKakao()}>
             <View style={styles.button}>
                 <Text style={styles.buttonText}>카카오 로그인</Text>
             </View>
         </TouchableOpacity>
         </View>
-    )
-}
-
-export default Login;
+    );
+  }
+  
+  export default Login;
