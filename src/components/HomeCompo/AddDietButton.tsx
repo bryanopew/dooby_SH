e649887098c1b20ae5c28b0e1f9531820d22c101
BRYRB <bryanopew@gmail.com>
2022-11-14 08:14:04 +0900

    design test by HS : main~

---
+++ src/components/HomeCompo/AddDietButton.tsx
@@ -0,0 +1,152 @@
+import {View, Text, Image} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import styled from 'styled-components/native';
+import {colors} from '~/constants/constants';
+import colorsHs from '~/styles/stylesHS/colors';
+import {useDispatch} from 'react-redux';
+import {useSelector} from 'react-redux';
+import {addCart, selectCart} from '~/stores/slices/addDietSlice';
+import {removeAll} from '~/stores/slices/basketSlice';
+import {cleanCalorieBar} from '~/stores/slices/calorieBarSlice';
+import DropDownPicker from 'react-native-dropdown-picker';
+
+const Container = styled.View`
+  flex: 1;
+`;
+
+const AddDietButton = ({onRefresh}) => {
+  const dispatch = useDispatch();
+  const content = useSelector((state: RootState) => {
+    return state.basketProduct.cart;
+  });
+  const selectedCart = useSelector((state: RootState) => {
+    return state.addDiet.selected;
+  });
+  const dietContent = useSelector((state: RootState) => {
+    return state.addDiet.cartsArray;
+  });
+  const cartPage = useSelector((state: RootState) => {
+    return state.addDiet.selectedCartPage;
+  });
+  console.log('Home/cartPage', cartPage);
+  const picked = useSelector((state: RootState) => {
+    return state.addDiet.pick;
+  });
+  const [value, setValue] = useState();
+  const [open, setOpen] = useState(false);
+  const [state, setState] = useState(1);
+  const [items, setItems] = useState([
+    {
+      label: '식단 1',
+      value: 1,
+      icon: () => (
+        <Image
+          source={require('~/Assets/Images/24_searchCancel.png')}
+          style={{
+            transform: [{scale: 100}],
+          }}
+        />
+      ),
+    },
+    {label: '식단 추가하기', value: 'add'},
+  ]);
+  const createCart = () => {
+    dispatch(addCart(content));
+  };
+  //식단 추가하기 순서 정렬
+  const selectDiet = i => {
+    if (i === undefined) {
+      return [{calorie: 0, carb: 0, protein: 0, fat: 0}];
+    }
+    return console.log('성공', dietContent[i - 1]);
+  };
+  const changeItemOrder = function (list, targetIdx, moveValue) {
+    if (list.length < 0) return;
+    const newPosition = targetIdx + moveValue;
+    if (newPosition < 0 || newPosition >= list.length) return;
+    const tempList = JSON.parse(JSON.stringify(list));
+    const target = tempList.splice(targetIdx, 1)[0];
+    tempList.splice(newPosition, 0, target);
+    return tempList;
+  };
+  const orderedItems = changeItemOrder(items, 1, items.length - 2);
+  const onIncrease = () => {
+    return setState(prev => prev + 1);
+  };
+  const addDiet = () => {
+    onIncrease();
+    setItems([...items, {label: `식단 ${state + 1}`, value: state + 1}]);
+    createCart();
+    onRefresh();
+    dispatch(removeAll());
+    dispatch(cleanCalorieBar());
+    dispatch(selectCart(items.length));
+  };
+  useEffect(() => {
+    setValue(cartPage);
+    onRefresh();
+  }, [cartPage]);
+  const removediet = () => {};
+  return (
+    <Container>
+      <DropDownPicker
+        style={{
+          width: '100%',
+          height: 48,
+          borderWidth: 0,
+          paddingLeft: -10,
+        }}
+        textStyle={{fontSize: 16}}
+        labelStyle={{
+          fontSize: 20,
+          fontWeight: 'bold',
+          backgroundColor: colorsHs.blue,
+          marginRight: 10,
+        }}
+        dropDownContainerStyle={{
+          position: 'relative',
+          width: 200,
+          marginTop: -44,
+          borderRadius: 3,
+          borderWidth: 1,
+          elevation: 3,
+          borderColor: colors.inActivated,
+          backgroundColor: colors.white,
+        }}
+        listItemLabelStyle={{
+          color: colors.main,
+        }}
+        selectedItemContainerStyle={{
+          backgroundColor: 'white',
+        }}
+        selectedItemLabelStyle={{
+          color: colorsHs.main,
+        }}
+        arrowIconStyle={{marginLeft: -10}}
+        itemSeparator={true}
+        itemSeparatorStyle={{
+          backgroundColor: 'grey',
+        }}
+        placeholder="식단1"
+        open={open}
+        setOpen={setOpen}
+        value={value}
+        items={orderedItems}
+        onSelectItem={item => {
+          item.value === 'add'
+            ? addDiet()
+            : (dispatch(selectCart(item.value)), onRefresh());
+        }}
+        onChangeValue={value => {
+          value === 'add' ? setValue(items.length - 1) : setValue(value);
+        }}
+        setValue={setValue}
+        setItems={setItems}
+        listMode="SCROLLVIEW"
+        dropDownDirection="BOTTOM"
+      />
+    </Container>
+  );
+};
+
+export default AddDietButton;
---
+++ src/components/NutrientsBar/NutrientsProgress.tsx
@@ -0,0 +1,61 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import styled from 'styled-components/native';
+import colors from '~/styles/stylesHS/colors';
+import {WIDTH} from '~/constants/constants';
+import * as Progress from 'react-native-progress';
+import {Col} from '~/styles/stylesHS/styledConsts';
+
+const Container = styled.View`
+  width: ${(WIDTH - 32 - 8 * 3) / 4};
+  height: 70px;
+  justify-content: center;
+`;
+
+const ProgressBarTitle = styled.Text`
+  font-size: 12px;
+  color: ${colors.textMain};
+  text-align: left;
+`;
+const ProgressBarNumber = styled.Text`
+  font-size: 12px;
+  margin-top: 5px;
+  color: ${colors.textMain};
+  text-align: right;
+`;
+
+const indicatorColorsByTitle = {
+  '칼로리(g)': colors.main,
+  '탄수화물(g)': colors.blue,
+  '단백질(g)': colors.green,
+  '지방(g)': colors.orange,
+};
+
+/** props:
+ * 1. title '칼로리(g)' | '탄수화물(g)' | '단백질(g)' | '지방(g)'
+ * 2. numerator(일부값)
+ * 3. denominator(전체값) */
+const NutrientsProgress = ({title, numerator, denominator}) => {
+  const indicatorColor = indicatorColorsByTitle[title];
+  const progressWidth = (WIDTH - 32 - 8 * 3) / 4;
+  console.log('NutrientsProgress: ', indicatorColor);
+  return (
+    <Container>
+      <Col>
+        <ProgressBarTitle>{title}</ProgressBarTitle>
+        <Progress.Bar
+          style={{marginTop: 5}}
+          progress={numerator / denominator}
+          width={progressWidth}
+          height={4}
+          color={indicatorColor}
+          unfilledColor={colors.line}
+          borderWidth={0}
+        />
+        <ProgressBarNumber>{`${numerator}/${denominator}`}</ProgressBarNumber>
+      </Col>
+    </Container>
+  );
+};
+
+export default NutrientsProgress;
